aed555ad84e90f1448e13b1e164abc3612a6776f
yurii-polych <bighunt0@gmail.com>
2023-07-25 20:15:14 +0300

    add viewsets

--- insurance/views.py
+++ insurance/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from insurance.models import Client, Insurance
+from insurance.serializers import ClientSerializer, InsuranceSerializer
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+
+class ClientViewSet(ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class InsuranceViewSet(ModelViewSet):
+    queryset = Insurance.objects.all()
+    serializer_class = InsuranceSerializer
