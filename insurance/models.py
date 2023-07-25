4d8e8cfb01e02a1b3289d6333a4a9784db4dead8
yurii-polych <bighunt0@gmail.com>
2023-07-25 20:40:57 +0300

    add migration undate models.py

--- insurance/models.py
+++ insurance/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
 
-class Client(models.Model):
-    first_name = models.CharField(max_length=50, null=False)
-    last_name = models.CharField(max_length=100, null=False)
-    age = models.PositiveSmallIntegerField(null=False)
-    bank_id = models.BigIntegerField(unique=True, null=False)
-    # insurance = models.ForeignKey(Insurance, on_delete=models.CASCADE, null=False)
-
-    class Meta:
-        db_table = 'client'
-
-
 class Insurance(models.Model):
     type = models.CharField(max_length=30, null=False)
     name = models.CharField(max_length=100)
     price = models.PositiveIntegerField(null=False)
     period = models.SmallIntegerField(null=False)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, null=False)
+    # client = models.ForeignKey(Client, on_delete=models.CASCADE, null=False)
 
     class Meta:
         db_table = 'insurance'
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=100, null=False)
+    age = models.PositiveSmallIntegerField(null=False)
+    bank_id = models.BigIntegerField(unique=True, null=False)
+    insurance = models.ForeignKey(Insurance, on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        db_table = 'client'
