e54939aca9c96bf5985522e17375da7d7d0b6e7f
yurii-polych <bighunt0@gmail.com>
2023-07-29 17:34:01 +0300

    fix serializers.py

--- insurance/serializers.py
+++ insurance/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from insurance.models import Client, Insurance
 
 
-class ClientSerializer(serializers.Serializer):
+class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = '__all__'
 
 
-class InsuranceSerializer(serializers.Serializer):
+class InsuranceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Insurance
         fields = '__all__'
