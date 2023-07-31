2c0686bdabc37ac42d2d4196cbc77a5fafc3fbe2
yurii-polych <bighunt0@gmail.com>
2023-07-31 12:34:52 +0300

    update admin.py

--- insurance/admin.py
+++ insurance/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from insurance.models import Insurance, Client
 
 # Register your models here.
-admin.site.register(Insurance)
-admin.site.register(Client)
+# admin.site.register(Insurance)
+# admin.site.register(Client)
+
+
+@admin.register(Insurance)
+class InsuranceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type', 'name', 'price', 'period', ]
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'age', 'bank_id', 'insurance', ]
