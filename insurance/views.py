471964853f1d598d477722a27dd496ad91dbb43d
yurii-polych <bighunt0@gmail.com>
2023-08-18 11:59:39 +0300

    update views.py: add permission restrintions

--- insurance/views.py
+++ insurance/views.py
@@ -3,13 +3,16 @@ from insurance.serializers import ClientSerializer, InsuranceSerializer
 
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
 
 
 class ClientViewSet(ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class InsuranceViewSet(ModelViewSet):
     queryset = Insurance.objects.all()
     serializer_class = InsuranceSerializer
+    permission_classes = [permissions.IsAuthenticated]
